60387393d2ef2affcabef89ef74f54739e7d1427
murynka den <dmurinka@gmail.com>
2020-06-22 00:00:07 +0300

    added cmd clue

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import subprocess
 
+#netsh wlan show profiles  #CMD command to show all wifi profiles
+
 data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8', errors="backslashreplace").split('\n')
 profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 for i in profiles:
